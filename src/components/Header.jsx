6c4d2184e3fbdc6c8d3fd53416b6705507e5877a
AakashNarwade <aakashnarwade06@gmail.com>
2024-07-08 23:25:31 +0530

    header completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div className="p-6 bg-slate-100 border-b-2 shadow-lg text-center font-medium text-xl ">
+      Admin UI
+    </div>
+  );
+}
+
+export default Header;
